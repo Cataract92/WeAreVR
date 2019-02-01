eb4a13011ac3e5ea8b7e59b208d017901ebf08fb
Cataract92 <salatgurke92@hotmail.de>
2019-02-01 18:31:00 +0100

    first iteration of grabbing/scaling

---
+++ Assets/Custom/CustomHand.cs
@@ -0,0 +1,44 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+//
+// Purpose: The hands used by the player in the vr interaction system
+//
+//=============================================================================
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine.Events;
+using System.Threading;
+
+namespace Valve.VR.InteractionSystem
+{
+    //-------------------------------------------------------------------------
+    // Links with an appropriate SteamVR controller and facilitates
+    // interactions with objects in the virtual world.
+    //-------------------------------------------------------------------------
+    public class CustomHand : Hand
+    {
+        private new void FixedUpdate()
+        {
+            if (currentAttachedObject != null)
+            {
+                AttachedObject attachedInfo = currentAttachedObjectInfo.Value;
+                if (attachedInfo.attachedObject != null)
+                {
+                    if (attachedInfo.HasAttachFlag(AttachmentFlags.VelocityMovement))
+                    {
+                        var customThrowable = attachedInfo.attachedObject.GetComponent<CustomThrowable>();
+                        if (!customThrowable || !customThrowable.enableTwoHandScaling || !customThrowable.twoHandsAttached)
+                            UpdateAttachedVelocity(attachedInfo);
+                        else
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
